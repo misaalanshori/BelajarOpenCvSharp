a70a66827e2c75fe83aac4005b7230d74cf7f4b9
M Isa Al Anshori <isabungsu@gmail.com>
2026-08-23 11:57:24 +0700

    we got a camera feed 🔥🔥🔥
    i got a yoga thinkpad with two cameras, and the back camera is camera 0, so camera 1 is the front display camera.

---
+++ BelajarOpenCvSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenCvSharp;
+
+namespace BelajarOpenCvSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var camera = new VideoCapture(1);
+
+            while (!camera.IsOpened()) continue;
+
+            int width = (int)camera.Get(VideoCaptureProperties.FrameWidth);
+            int height = (int)camera.Get(VideoCaptureProperties.FrameHeight);
+
+            camera.AutoExposure = 1.0;
+
+
+            Mat currentFrame = new(
+                rows: height,
+                cols: width,
+                type: MatType.CV_8UC3,
+                s: Scalar.Black
+                );
+            while (camera.Read(currentFrame))
+            {
+                if (currentFrame.Empty())
+                {
+                    continue;
+                }
+
+                //Cv2.CvtColor(currentFrame, currentFrame, ColorConversionCodes.BGR2GRAY);
+
+                Cv2.ImShow("Video Feed", currentFrame);
+                if (Cv2.WaitKey(1) == 27) break;
+            }
+
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
